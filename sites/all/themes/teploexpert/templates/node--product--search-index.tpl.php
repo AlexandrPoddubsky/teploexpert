c9b0dd71ceb1b3c5d481bbae773a79a74b274356
Alex <gun_dose@tut.by>
2017-01-31 21:23:01 +0300

    field display in product index is adjustable now

--- sites/all/themes/teploexpert/templates/node--product--search-index.tpl.php
+++ sites/all/themes/teploexpert/templates/node--product--search-index.tpl.php
@@ -6,6 +6,7 @@
  * Complete documentation for this file is available online.
  * @see https://drupal.org/node/1728164
  */
+$content['field_sku']['#label_display'] = 'hidden';
 ?>
       <div class="views-field views-field-title"><a href="<?php print $node_url; ?>"><?php print $title; ?></a></div>
       <div class="views-field views-field-field-sku">    <span class="views-label views-label-field-sku">Артикул: </span>    <span class="field-content"><?php
@@ -15,6 +16,12 @@
         <?php
         print render($content['uc_product_image']);
         ?></div>
+        <?php
+            hide($content['display_price']);
+            hide($content['add_to_cart']);
+            hide($content['compare']);
+        ?>
+    <?php if (3 ==5) : ?>
         <div class="views-field views-field-dimensions"><span class="views-label views-label-field-dimensions">ВхШхГ, мм: </span>
         <?php
         print render($content['field_dimensions']);
@@ -29,6 +36,8 @@
         print render($content['field_power']);
         ?></div>
 		<?php endif; ?>
+    <?php endif; ?>
+        <?php print render($content); ?>
         <?php print render($content['compare']); ?>
         <div class="views-field views-field-price"><span class="views-label views-label-price">Цена: </span>
         <?php
